2aa9318ee1b6fb396bd014843c4f087b2214f0fc
joaccocaballero <joaquincaballero182@gmail.com>
2022-09-02 20:02:44 -0300

    modal finalizado

---
+++ main.js
@@ -0,0 +1,13 @@
+const openModal = document.getElementById("displayModal")
+const modal = document.querySelector(".modal")
+const closeModal = document.querySelector(".closeModal")
+
+openModal.addEventListener("click", (e) => {
+    e.preventDefault()
+    modal.classList.add("modal--show")
+})
+
+closeModal.addEventListener("click", (e) => {
+    e.preventDefault()
+    modal.classList.remove("modal--show")
+})
